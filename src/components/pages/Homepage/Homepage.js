a4d58901c91d8845aef00fba3f0f0fcc77b97f69
James Seymour-Lock <james@simpleasmilk.co.uk>
2016-05-11 18:45:57 +0100

    Implement React Helmet

--- src/components/pages/Homepage/Homepage.js
+++ src/components/pages/Homepage/Homepage.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import Helmet from "react-helmet";
 
 // Styles
 import styles from './Homepage.css';
@@ -7,7 +8,15 @@ import m from 'global/modifiers';
 export default class Homepage extends Component {
   render() {
     return(
-      <h1>Simple as Milk</h1>
+        <div>
+            <Helmet
+                title="Simple as Milk"
+                meta={[
+                    {"name": "description", "content": "Helmet application"}
+                ]}
+            />
+            <h1>Simple as Milk</h1>
+        </div>
     )
   }
 }
